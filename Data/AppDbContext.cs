b07134657cefb7561904a85c44c949feeaaa0415
AHsytraka <ahsytraka@gmail.com>
2023-03-05 21:30:08 +0300

    ensure

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -6,6 +6,13 @@ namespace Devhunt.Data;
 #nullable disable
 public class AppDbContext : DbContext
 {
+    public DbSet<Pub> Pubs {get; set;}
+    public DbSet<User> Users {get; set;}
+    public DbSet<Lesson> Lessons {get; set;}
+    public DbSet<ListTemp> ListTemps {get; set;}
+    public DbSet<Document> Documents {get; set;}
+    public DbSet<Reaction> Reactions {get; set;}
+    public DbSet<Commentaire> Commentaires {get; set;}
     protected readonly IConfiguration _configuration;
 
     public AppDbContext(IConfiguration configuration)
@@ -21,14 +28,6 @@ public class AppDbContext : DbContext
         options.UseMySql(connectionString,  ServerVersion.AutoDetect(connectionString));
         Database.EnsureCreated();
     }
-
-    public DbSet<Pub> Pubs {get; set;}
-    public DbSet<User> Users {get; set;}
-    public DbSet<Lesson> Lessons {get; set;}
-    public DbSet<ListTemp> ListTemps {get; set;}
-    public DbSet<Document> Documents {get; set;}
-    public DbSet<Reaction> Reactions {get; set;}
-    public DbSet<Commentaire> Commentaires {get; set;}
         protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<User>(entity =>
